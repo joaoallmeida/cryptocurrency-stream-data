9960c7f746aeb688514c5a6384af938074935b6f
joao soares <jaumalmeida@outlook.com>
2022-11-25 22:49:50 -0300

    Updated file producerData.py

--- scripts/producerData.py
+++ scripts/producerData.py
@@ -1,52 +1,53 @@
-from os import environ
 from kafka import KafkaProducer
-
 import requests
 import json
 import time
 import logging
-from websocket import create_connection
-
 
 BOOSTRAP_SERVER = 'localhost:9093'
 TOPIC_NAME = 'financial-data'
 URL_BASE = 'https://api.coincap.io/v2'
-SOCKET_BASE = 'wss://ws.coincap.io/prices?assets=ALL'
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s -> %(message)s',datefmt='%y-%m-%d %H:%M:%S')
+logConf = logging.getLogger()
+logConf.setLevel(logging.INFO)
 
-producer = KafkaProducer(
-                    bootstrap_servers=BOOSTRAP_SERVER
-                    ,value_serializer=lambda v: json.dumps(v).encode('utf-8')
-                )
+def producerDataStream():
 
-statusCode = 200
-
-while statusCode == 200 :
-    
-    logging.info('Getting data from source!')
-    
     try:      
+        producer = KafkaProducer(
+                            bootstrap_servers=BOOSTRAP_SERVER
+                            ,value_serializer=lambda v: json.dumps(v).encode('utf-8')
+                        )
+
+        statusCode = 200
+
+        while statusCode == 200 :
             
-        response = requests.get(f'{URL_BASE}/assets?limit=2000',stream=True)
-        
-        response.raise_for_status()
-        
-        statusCode = response.status_code
-        
-        for line in response.iter_lines():
-            if line:
-                
-                logging.info('Data is available for consume!')
-                dataMessage = json.loads(line)
+            logging.info('Getting data from source!')
+              
+            response = requests.get(f'{URL_BASE}/assets?limit=2000',stream=True)
+            response.raise_for_status()
+            statusCode = response.status_code
+            
+            for line in response.iter_lines():
+                if line:
+                    
+                    logging.info('Data is available for producer!')
+                    dataMessage = json.loads(line)
 
-                logging.info(f'Send data to kafka topic - {TOPIC_NAME}')
+                    logging.info(f'Send data to kafka topic - {TOPIC_NAME}')
 
-                producer.send(TOPIC_NAME,  dataMessage)
-        
+                    producer.send(TOPIC_NAME,  dataMessage)
+                    producer.flush()
+            
+            logging.warning('Waiting 1 minute to place a new request!')
+            time.sleep(60)
+            
     except Exception as e:
-        logging.error(f'Data message failed.')
+        logging.error(f'Failed to capture API data!')
         raise e
-
-    logging.warning('Waiting 1 minute to place a new request!')
-    time.sleep(60)
+        
+if __name__=="__main__":
+    
+    producerDataStream()
