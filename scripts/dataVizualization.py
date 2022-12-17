b917e5cc11b9d890032380e75cd50342b8f2a2c3
joao soares <jaumalmeida@outlook.com>
2022-12-17 18:54:30 -0300

    updated dataVizualization.py

--- scripts/dataVizualization.py
+++ scripts/dataVizualization.py
@@ -20,7 +20,7 @@ st.set_page_config(
     }
 )
 
-# autoRefresh = st_autorefresh(interval=60000,key="fizzbuzzcounter")
+autoRefresh = st_autorefresh(interval=60000,key="fizzbuzzcounter")
 
 @st.experimental_singleton
 def connMongo():
@@ -33,7 +33,8 @@ def connMongo():
     
     return mongoClient
 
-@st.cache(allow_output_mutation=True)    
+ 
+@st.experimental_memo(ttl=30) 
 def getCryptoData():
     
     mongoClient = connMongo()
@@ -51,18 +52,16 @@ def getCryptoData():
 df = getCryptoData()
 
 ## Filter
-col01 , col02 = st.columns(2)
+with st.sidebar:
+#     dateFilterStart , dateFilterEnd = st.date_input('Filter by Date',value=[df['timestamp'].min(),df['timestamp'].max()])
+    symbolFilter = st.selectbox('Select The Symbol',options=df['symbol'].unique())
 
-with col01:
-    dateFilter = st.date_input('Filter by Date',value=df['timestamp'].max(), min_value=df['timestamp'].min())
-with col02:
-    symbolFilter = st.selectbox('Select The Symbol',options=df['symbol'].head(100).unique())
+    st.markdown('---')
 
-st.markdown('---')
 
 dfSelection = df[ 
-    ((df['timestamp'].dt.date == dateFilter)) #& (df['timestamp'].dt.date <= dateFilterEnd)) 
-    & (df['symbol'] == symbolFilter) 
+    #    ((df['timestamp'].dt.date == dateFilterStart) & (df['timestamp'].dt.date <= dateFilterEnd)) 
+     (df['symbol'] == symbolFilter) 
 ]
 
 
@@ -76,12 +75,22 @@ marketCapUsd = dfSelection.sort_values("timestamp",ascending=False)['marketCapUs
 volumeUsd24Hr = float(str( dfSelection.sort_values("timestamp",ascending=False)['volumeUsd24Hr'].values[0]))
 changePercent = dfSelection.sort_values("timestamp",ascending=False)['changePercent24Hr'].values[0]
 
+
+with st.sidebar:
+    
+    st.title(f'{symbolFilter} to USD Converter')
+    
+    numberConvert = st.number_input(f'{symbolFilter}',format='%f',key='convertNumber')
+    calculated = (numberConvert * currentPrice)
+    convertResult = st.number_input('USD',value=calculated,key='result')
+    
+    
 st.markdown('<h1 style="text-align:center">Market Info</h1>',unsafe_allow_html=True)
 
 col01, col02, col03 ,col04, col05, col06,col07 = st.columns([5,10,7,10,10,12,12])
 
 with col01:
-    st.image(f'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/{symbolFilter.lower()}.png')
+    st.image(f'https://cryptoicons.org/api/color/{symbolFilter.lower()}/128')
 with col02:
     st.markdown(f'''## {cryptoName}''')
     st.markdown(f'###### Ranking Position #{cryptoPosition}')
@@ -99,14 +108,17 @@ with col07:
     
 st.markdown("---")
 
-tab01, tab02 = st.tabs([':heavy_dollar_sign: Price USD',':moneybag: Market Cap USD'])
-
-timesSeriesByPrice = dfSelection.pivot_table(index='timestamp',columns='symbol',values='priceUsd').reset_index()
-timesSeriesByMarketCap = dfSelection.pivot_table(index='timestamp', columns='symbol', values='marketCapUsd').reset_index()
+## Charts
 
 config = dict({"displayModeBar":'hover',"scrollZoom":False,"displaylogo":False})
 
 with st.container():
+    tab01, tab02 = st.tabs([':heavy_dollar_sign: Price USD',':moneybag: Market Cap USD'])
+
+    timesSeriesByPrice = dfSelection.pivot_table(index='timestamp',columns='symbol',values='priceUsd').reset_index()
+    timesSeriesByMarketCap = dfSelection.pivot_table(index='timestamp', columns='symbol', values='marketCapUsd').reset_index()
+
+    
     with tab01:
         st.subheader(':heavy_dollar_sign: Price USD')
 
@@ -119,7 +131,47 @@ with st.container():
         
 
         figLineChart.update_traces(mode='lines')
-        figLineChart.update_layout(showlegend=False,yaxis_title=None,xaxis_title=None)
+        figLineChart.update_layout(
+            showlegend=False
+            ,yaxis_title=None
+            ,xaxis_title=None
+            ,xaxis=dict(
+                    rangeselector=dict(
+                        buttons=list([
+                            dict(count=12,
+                                label="12H",
+                                step="hour",
+                                stepmode="backward"),
+                            dict(count=1,
+                                label="1D",
+                                step="day",
+                                stepmode="backward"),
+                            dict(count=7,
+                                label="7D",
+                                step="day",
+                                stepmode="backward"),
+                            dict(count=1,
+                                label="1M",
+                                step="month",
+                                stepmode="backward"),
+                            dict(count=3,
+                                label="3M",
+                                step="month",
+                                stepmode="backward"),
+                            dict(count=1,
+                                label="YTD",
+                                step="year",
+                                stepmode="todate"),
+                            dict(label='ALL',
+                                 step="all")
+                        ])
+                    ),
+                    rangeslider=dict(
+                        visible=True
+                    ),
+                    type="date"
+                )
+        )
 
         st.plotly_chart(figLineChart,theme='streamlit',use_container_width=True,config=config)
 
@@ -133,27 +185,55 @@ with st.container():
         )
         
         figLineChart.update_traces(mode='lines')
-        figLineChart.update_layout(showlegend=False,yaxis_title=None,xaxis_title=None)
+        figLineChart.update_layout(
+            showlegend=False
+            ,yaxis_title=None
+            ,xaxis_title=None
+            ,xaxis=dict(
+                    rangeselector=dict(
+                        buttons=list([
+                            dict(count=12,
+                                label="12H",
+                                step="hour",
+                                stepmode="backward"),
+                            dict(count=1,
+                                label="1D",
+                                step="day",
+                                stepmode="backward"),
+                            dict(count=7,
+                                label="7D",
+                                step="day",
+                                stepmode="backward"),
+                            dict(count=1,
+                                label="1M",
+                                step="month",
+                                stepmode="backward"),
+                            dict(count=3,
+                                label="3M",
+                                step="month",
+                                stepmode="backward"),
+                            dict(count=1,
+                                label="YTD",
+                                step="year",
+                                stepmode="todate"),
+                            dict(label='ALL',
+                                 step="all")
+                        ])
+                    ),
+                    rangeslider=dict(
+                        visible=True
+                    ),
+                    type="date"
+                )
+        )
         
         st.plotly_chart(figLineChart,theme='streamlit',use_container_width=True,config=config)
 
-
-    with st.sidebar:
-        
-        st.title(f'{symbolFilter} to USD Converter')
-        
-        
-        numberConvert = st.number_input(f'{symbolFilter}',format='%f')
-        
-        calculated = (numberConvert * currentPrice)
-        calculatedResult = f'{calculated:,.2f}' if calculated >= 1 else f'{calculated:,.5f}'
-    
-        convertResult = st.text_input('USD',calculatedResult,disabled=False)
 
 ## Ranking    
 # rankingDf = df[df['timestamp'].dt.date == datetime.now().date()].iloc[:,1:10].reset_index()
 # idx = rankingDf.groupby(['name'])['index'].transform(max) == rankingDf['index']
 # rankingDf = rankingDf[idx].drop('index',axis=1)
 
-# st.subheader('Cryptocurency Rankings')
+# st.subheader('Cryptocurency Ranking')
 # st.table(rankingDf)
